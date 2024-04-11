b61aa97746fadd9c14b6cd1259cff4cca9868b91
Luis Carvalho <lapc19@hotmail.com>
2024-04-11 23:46:36 +0100

    update

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
 	"log"
 
 	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type Game struct {
@@ -19,28 +20,47 @@ type Game struct {
 	numberOfCellsWidth  int
 	numberOfCellsHeight int
 	cellSize            int
-	testImage           *image.RGBA
+	image               *image.RGBA
+	isPaused            bool
 }
 
 type GameCell struct {
 	value           int
-	image           *ebiten.Image
-	imageOptions    *ebiten.DrawImageOptions
 	neighborsValues [8]*int
 }
 
 func (g *Game) Update() error {
 
-	//fmt.Println(ebiten.ActualFPS())
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		return ebiten.Termination
+	}
 
-	for i := 0; i < g.numberOfCellsWidth; i++ {
-		for j := 0; j < g.numberOfCellsHeight; j++ {
+	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
+		ebiten.SetFullscreen(!ebiten.IsFullscreen())
+	}
 
-			//neighbors := GetNeighbors(i, j, g.numberOfCellsWidth, g.numberOfCellsHeight)
-			//aliveNeighbors := GetAliveNeighbors(g.gameMap, neighbors)
-			aliveNeighbors := GetAliveNeighborsPointers(g.gameMap[i][j].neighborsValues)
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		g.isPaused = !g.isPaused
+	}
 
-			g.tempMap[i][j] = DecideCellFuture(g.gameMap[i][j].value, aliveNeighbors)
+	if !g.isPaused {
+		for i := 0; i < g.numberOfCellsWidth; i++ {
+			for j := 0; j < g.numberOfCellsHeight; j++ {
+				aliveNeighbors := GetAliveNeighborsPointers(g.gameMap[i][j].neighborsValues)
+				g.tempMap[i][j] = DecideCellFuture(g.gameMap[i][j].value, aliveNeighbors)
+			}
+		}
+	}
+
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0) {
+		x, y := ebiten.CursorPosition()
+		var i int = y / g.cellSize
+		var j int = x / g.cellSize
+
+		if g.tempMap[i][j] == 0 {
+			g.tempMap[i][j] = 1
+		} else {
+			g.tempMap[i][j] = 0
 		}
 	}
 
@@ -49,109 +69,33 @@ func (g *Game) Update() error {
 
 			g.gameMap[i][j].value = g.tempMap[i][j]
 
-			/*
-				if g.gameMap[i][j].value == 1 {
-					g.gameMap[i][j].image.Fill(color.White)
-				} else {
-					g.gameMap[i][j].image.Fill(color.Black)
-				}
-			*/
-		}
-	}
+			var colour uint8 = 0
 
-	/*
-		[0]
-		0 1 2 3 4 5 6 7 8 9
+			if g.gameMap[i][j].value == 1 {
+				colour = 255
+			}
 
+			for r := 0; r < g.cellSize; r++ {
+				for t := 0; t < g.cellSize; t++ {
 
-	*/
+					g.image.Pix[4*((g.screenWidth*i*g.cellSize+(t*g.screenWidth))+(j*g.cellSize+r))] = uint8(colour)
+					g.image.Pix[4*((g.screenWidth*i*g.cellSize+(t*g.screenWidth))+(j*g.cellSize+r))+1] = uint8(colour)
+					g.image.Pix[4*((g.screenWidth*i*g.cellSize+(t*g.screenWidth))+(j*g.cellSize+r))+2] = uint8(colour)
+					g.image.Pix[4*((g.screenWidth*i*g.cellSize+(t*g.screenWidth))+(j*g.cellSize+r))+3] = 0xff
 
-	for i := 0; i < g.numberOfCellsWidth; i++ {
-		for j := 0; j < g.numberOfCellsHeight; j++ {
-
-			//isBlack := g.gameMap[i][j].value == 0
-
-			for r := 0; r < 10; r++ {
-				for t := 0; t < 10; t++ {
-
-					/*
-						g.testImage.Pix[4*(i*10+r+g.screenWidth*4*j+t)] = uint8(255)
-						g.testImage.Pix[4*(i*10+1+r+g.screenWidth*4*j+t)] = uint8(0)
-						g.testImage.Pix[4*(i*10+2+r+g.screenWidth*4*j+t)] = uint8(0)
-						g.testImage.Pix[4*(i*10+3+r+g.screenWidth*4*j+t)] = 0xff
-					*/
-
-					g.testImage.Pix[4*(g.screenWidth*i*10+j*r)] = uint8(255)
-					g.testImage.Pix[4*(g.screenWidth*i*10+j*r)+1] = uint8(0)
-					g.testImage.Pix[4*(g.screenWidth*i*10+j*r)+2] = uint8(0)
-					g.testImage.Pix[4*(g.screenWidth*i*10+j*r)+3] = 0xff
 				}
 			}
 
-			/*
-				g.testImage.Pix[4*(g.screenWidth*i*10+j*10)] = uint8(255)
-				g.testImage.Pix[4*(g.screenWidth*i*10+j*10)+1] = uint8(0)
-				g.testImage.Pix[4*(g.screenWidth*i*10+j*10)+2] = uint8(0)
-				g.testImage.Pix[4*(g.screenWidth*i*10+j*10)+3] = 0xff
-			*/
+			g.image.Pix[4*(g.screenWidth*i*g.cellSize+j*g.cellSize)] = uint8(0)
+			g.image.Pix[4*(g.screenWidth*i*g.cellSize+j*g.cellSize)+1] = uint8(0)
+			g.image.Pix[4*(g.screenWidth*i*g.cellSize+j*g.cellSize)+2] = uint8(255)
+			g.image.Pix[4*(g.screenWidth*i*g.cellSize+j*g.cellSize)+3] = 0xff
 
 		}
 
 	}
-
-	/*
-		var l = g.screenWidth * g.screenHeight
-		fmt.Println(l)
-		fmt.Println(len(g.testImage.Pix))
-
-		for i := 0; i < l; i++ {
-			g.testImage.Pix[4*i] = uint8(i / g.screenWidth)
-			g.testImage.Pix[4*i+1] = uint8(i / g.screenHeight)
-			g.testImage.Pix[4*i+2] = uint8(i % g.screenWidth)
-			g.testImage.Pix[4*i+3] = 0xff
-		}*/
 
 	return nil
-}
-
-type CellPosition struct {
-	i int
-	j int
-}
-
-func GetNeighbors(i int, j int, maxWidth int, maxHeight int) [8]CellPosition {
-	neighbors := [8]CellPosition{}
-
-	/*
-		(i-1, j-1) (i, j-1) (i+1, j-1)
-		(i-1, j)   (i, j)   (i+1, j)
-		(i-1, j+1) (i, j+1) (i+1, j+1)
-	*/
-
-	neighbors[0] = CellPosition{i: i - 1, j: j - 1}
-	neighbors[1] = CellPosition{i: i, j: j - 1}
-	neighbors[2] = CellPosition{i: i + 1, j: j - 1}
-	neighbors[3] = CellPosition{i: i - 1, j: j}
-	neighbors[4] = CellPosition{i: i + 1, j: j}
-	neighbors[5] = CellPosition{i: i - 1, j: j + 1}
-	neighbors[6] = CellPosition{i: i, j: j + 1}
-	neighbors[7] = CellPosition{i: i + 1, j: j + 1}
-
-	for i = 0; i < len(neighbors); i++ {
-		if neighbors[i].i < 0 || neighbors[i].i >= maxWidth {
-			neighbors[i].i = -1
-			neighbors[i].j = -1
-			continue
-		}
-
-		if neighbors[i].j < 0 || neighbors[i].j >= maxHeight {
-			neighbors[i].i = -1
-			neighbors[i].j = -1
-			continue
-		}
-	}
-
-	return neighbors
 }
 
 func GetNeighborsPointers(gameMap [][]GameCell, i int, j int, maxWidth int, maxHeight int) [8]*int {
@@ -213,24 +157,6 @@ func GetAliveNeighborsPointers(neighborsPointers [8]*int) int {
 	return alive
 }
 
-func GetAliveNeighbors(gameMap [][]GameCell, neighbors [8]CellPosition) int {
-	var alive int = 0
-
-	for i := 0; i < len(neighbors); i++ {
-		if neighbors[i].i == -1 || neighbors[i].j == -1 {
-			continue
-		}
-
-		var value int = gameMap[neighbors[i].i][neighbors[i].j].value
-
-		if value == 1 {
-			alive++
-		}
-	}
-
-	return alive
-}
-
 func DecideCellFuture(cellValue int, aliveCells int) int {
 	if cellValue == 1 {
 		if aliveCells < 2 || aliveCells > 3 {
@@ -248,18 +174,12 @@ func DecideCellFuture(cellValue int, aliveCells int) int {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.WritePixels(g.image.Pix)
 
-	/*
-		for i := 0; i < g.numberOfCellsWidth; i++ {
-			for j := 0; j < g.numberOfCellsHeight; j++ {
-				screen.DrawImage(g.gameMap[i][j].image, g.gameMap[i][j].imageOptions)
-
-				screen.WritePixels()
-			}
-		}
-	*/
-
-	screen.WritePixels(g.testImage.Pix)
+	// Draw the message.
+	tutorial := "Space: Move forward\nLeft/Right: Rotate"
+	msg := fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f\n%s", ebiten.ActualTPS(), ebiten.ActualFPS(), tutorial)
+	ebitenutil.DebugPrint(screen, msg)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth int, screenHeight int) {
@@ -267,10 +187,8 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth int, screenH
 }
 
 func main() {
-	fmt.Println("start")
-
 	var cellSize int = 10
-	var screenWidth int = 2000
+	var screenWidth int = 1000
 	var screenHeight int = 1000
 	var numberOfCellsWidth int = screenHeight / cellSize
 	var numberOfCellsHeight int = screenWidth / cellSize
@@ -283,27 +201,14 @@ func main() {
 		numberOfCellsWidth:  numberOfCellsWidth,
 		numberOfCellsHeight: numberOfCellsHeight,
 		cellSize:            cellSize,
-		testImage:           image.NewRGBA(image.Rect(0, 0, screenWidth, screenHeight)),
+		image:               image.NewRGBA(image.Rect(0, 0, screenWidth, screenHeight)),
+		isPaused:            false,
 	}
 
 	for i := 0; i < numberOfCellsWidth; i++ {
 		for j := 0; j < numberOfCellsHeight; j++ {
-			var cellValue int = rand.Intn(2)
-			image := ebiten.NewImage(game.cellSize, game.cellSize)
-			if cellValue == 1 {
-				image.Fill(color.White)
-			} else {
-				image.Fill(color.Black)
-			}
-
-			position := ebiten.GeoM{}
-			position.Translate(float64(j)*float64(cellSize), float64(i)*float64(cellSize))
-			drawImageOptions := ebiten.DrawImageOptions{GeoM: position}
-
 			gameCell := GameCell{
-				value:        rand.Intn(2),
-				image:        image,
-				imageOptions: &drawImageOptions,
+				value: rand.Intn(2),
 			}
 			game.gameMap[i] = append(game.gameMap[i], gameCell)
 			game.tempMap[i] = append(game.tempMap[i], 0)
@@ -312,9 +217,7 @@ func main() {
 
 	for i := 0; i < numberOfCellsWidth; i++ {
 		for j := 0; j < numberOfCellsHeight; j++ {
-
 			neighborsPointers := GetNeighborsPointers(game.gameMap, i, j, game.numberOfCellsWidth, game.numberOfCellsHeight)
-
 			game.gameMap[i][j].neighborsValues = neighborsPointers
 
 		}
@@ -322,6 +225,8 @@ func main() {
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Game Of Life")
+
+	ebiten.SetFullscreen(true)
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
